797a4b28fd3b01c226703b0a2778b64e3d334412
pedroCurioni <pedrocurioni@gmail.com>
2022-06-26 12:47:17 -0300

    10 da lista 1 usando for

--- lista1/10.py
+++ lista1/10.py
@@ -2,6 +2,14 @@ import math
 
 x = float(input("Digite um valor: "))
 
-sen = x - x**3/math.factorial(3) + x**5/math.factorial(5) +x**7/math.factorial(7) +x**9/math.factorial(9) +x**11/math.factorial(11) +x**13/math.factorial(13) + x**15/math.factorial(15) + x**17/math.factorial(17) + x**21/math.factorial(21)
+sen = 0
+passo = 0
+
+for i in range(1, 21, 2):
+    if(passo % 2 == 0):
+        sen = sen + x**i/math.factorial(i)
+    elif(passo % 2 != 0):
+        sen = sen - x**i/math.factorial(i)
+    passo += 1
 
 print(sen)
